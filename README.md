7d247cb47e390fdbe6989dea0910a0fbcb176e3c
Dmitriy <dmitriy.sai2013@gmail.com>
2021-03-26 16:38:17 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LocalStoreCoreData
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import LocalStoreCoreDataTests
+
+var tests = [XCTestCaseEntry]()
+tests += LocalStoreCoreDataTests.allTests()
+XCTMain(tests)
